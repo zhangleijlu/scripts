8ff75a4910e04da48a63b2c23bb87228e1a83736
zhanglei02 <zhanglei02@zhanglei02s-MacBook-Pro.local>
2018-07-05 16:23:07 +0800

    mid work

--- delete_rows.php
+++ delete_rows.php
@@ -6,7 +6,7 @@
  * Time: 下午4:10
  */
 $old_mysqli = new mysqli("67.218.158.33","root","root", "content_ori");
-$sql = "select ori_art_id,min(insert_time),site as min_insert_time from content_ori_201804_newlife101 group by site,ori_art_id having count(*) >1";
+$sql = "select ori_art_id,min(insert_time) as min_insert_time ,site  from content_ori_201804_newlife101 group by site,ori_art_id having count(*) >1";
 $ret = $old_mysqli->query($sql);
 while ($arr = $ret->fetch_assoc()){
     $ori_art_id = $arr['ori_art_id'];
