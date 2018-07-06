0dcfc592cf21dc572f4769e3f40227e059a8a80b
zhanglei02 <zhanglei02@zhanglei02s-MacBook-Pro.local>
2018-07-06 15:54:15 +0800

    mid work

--- string_replace.php
+++ string_replace.php
@@ -8,6 +8,15 @@
 $old_mysqli = new mysqli("180.76.174.128","root","asdf", "yascmf_base");
 $old_mysqli->query("set names utf8");
 for ($i=0;$i<100;$i++){
-    $sql = " update yascmf_articles_$i set content=REPLACE(content, \"data-original=\", \"src=\")";
-    $old_mysqli->query($sql);
+    $sql = "SELECT * FROM  yascmf_articles_$i WHERE author_name= \"businessweekly\")";
+    $ret = $old_mysqli->query($sql);
+    while ($arr = $ret->fetch_assoc()){
+        $content = $arr['content'];
+        $id = $arr['id'];
+        if(strpos($content, "data-original=") !==false){
+            $new_content = str_replace("data-original=", "", $content);
+        }
+        $update_sql = "UPDATE yascmf_articles_$i set content = '$new_content' WHERE `id`='$id'";
+        $old_mysqli->query($update_sql);
+    }
 }
