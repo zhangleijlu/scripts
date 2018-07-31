aefed8387cdbef3ee963890f2a83c7c70c80bb04
zhanglei02 <zhanglei02@zhanglei02s-MacBook-Pro.local>
2018-07-31 15:13:58 +0800

    1

--- transparent_ip.php
+++ transparent_ip.php
@@ -12,7 +12,7 @@ if(preg_match('/<tbody>(.*?)<\/tbody>/', $html, $matches)){
 $fh = fopen('transparent_ip.txt', 'w');
 if(preg_match_all('/<tr>(.*?)<\/tr>/', $tbody, $matches)){
     foreach ($matches[1] as $tr){
-        preg_match_all('/<td>(.*?)<\/td>/', $tr, $tr_matches); var_dump($tr_matches);
+        preg_match_all('/<td>(.*?)<\/td>/', $tr, $tr_matches);
         if($tr_matches[1][3] == 'transparent'){
             $ip = $tr_matches[1][0];
             $port = $tr_matches[1][1];
@@ -37,6 +37,11 @@ function check($ip, $port){
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "GET",
     ));
+    @curl_setopt($curl, CURLOPT_HEADER  , true);  // we want headers
+    @curl_setopt($curl, CURLOPT_NOBODY  , true);  // we don't need body
+
+
+
 
     $response = curl_exec($curl);
     $err = curl_error($curl);
@@ -44,8 +49,8 @@ function check($ip, $port){
     curl_close($curl);
 
     if ($err) {
-        echo "cURL Error #:" . $err;
+        return false;
     } else {
-        echo $response;
+        return true;
     }
 }
